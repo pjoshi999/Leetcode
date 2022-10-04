b8aa4d1a78a33933814a05b64425c9a69c851c9f
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2022-10-04 19:52:14 +0530

    Time: 8 ms (36.02%), Space: 7.9 MB (15.53%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        map<int, int> mp;
+        vector<int> v, ans;
+        
+        for(int i=0; i<arr1.size(); i++) {
+            mp[arr1[i]]++;
+        }
+        for(int i=0; i<arr2.size(); i++) {
+            int key = arr2[i];
+            auto temp = mp.find(key);
+            while(mp.find(key) != mp.end() && temp->second > 0) {
+                ans.push_back(temp->first);
+                temp->second -= 1;
+            }
+        }
+        
+        for(auto it=mp.begin(); it!=mp.end(); it++) {
+            while(it->second > 0 ) {
+                v.push_back(it->first);
+                it->second-=1;
+            }
+        }
+        sort(v.begin(), v.end());
+        ans.insert( ans.end(), v.begin(), v.end() );
+        return ans;
+    }
+};
