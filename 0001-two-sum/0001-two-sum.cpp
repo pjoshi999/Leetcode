67e0e9e2d3db46c0ed3b8853d57488d02158571b
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2022-11-04 00:53:19 +0530

    Time: 29 ms (60.58%), Space: 10.8 MB (51.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,11 +4,12 @@ public:
         unordered_map<int, int> mp;
         vector<int> v;
         for(int i=0; i<nums.size(); i++) {
-            int key = target-nums[i];
+            int key = target - nums[i];
             auto temp = mp.find(key);
             if(mp.find(key) != mp.end()) {
                 v.push_back(i);
                 v.push_back(temp->second);
+                break;
             }
             mp[nums[i]] = i;
         }
