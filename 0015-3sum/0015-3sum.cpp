ac644ac17e0c0462fac61215f914427505fbd82f
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2022-11-05 23:23:05 +0530

    Time: 129 ms (77.39%), Space: 21.1 MB (39.19%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,12 +2,21 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
+        
+        // Sort The array
         sort(nums.begin(), nums.end());
+        
+        // moves for a
         for(int i=0; i<nums.size()-2; i++) {
+            
+            // duplicates are ignored
             if(i==0 || (i>0 && nums[i] != nums[i-1])) {
-                int start = i+1;
-                int end = nums.size()-1;
-                int target = 0-nums[i];    // a+b+c=0  so  b + c = -a;
+                
+                int start = i+1;  // b 
+                int end = nums.size()-1;  // c         
+                
+                // a+b+c=0  so  b + c = -a;
+                int target = 0-nums[i];    
             
                 // a = nums[i]  b = nums[start]  c = nums[end]
                 while(start < end) {
@@ -22,7 +31,10 @@ public:
                         vector<int> v = {nums[i], nums[start], nums[end]};
                         res.push_back(v);
                         
+                        // Ignoring the duplicates for b
                         while(start < end && nums[start] == nums[start+1]) start++;
+
+                        // Ignoring the duplicates for c
                         while(start < end && nums[end] == nums[end-1]) end--;
                         start++;
                         end--;
