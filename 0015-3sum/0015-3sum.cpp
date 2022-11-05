5fcaa8bd2792c2c52014e0cd9892b1a1cfa82f60
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2022-11-05 22:36:56 +0530

    Time: 73 ms (97.76%), Space: 21 MB (46.09%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,10 +5,11 @@ public:
         sort(nums.begin(), nums.end());
         for(int i=0; i<nums.size()-2; i++) {
             if(i==0 || (i>0 && nums[i] != nums[i-1])) {
-                int start=i+1;
-                int end=nums.size()-1;
-                int target = 0-nums[i];
-
+                int start = i+1;
+                int end = nums.size()-1;
+                int target = 0-nums[i];    // a+b+c=0  so  b + c = -a;
+            
+                // a = nums[i]  b = nums[start]  c = nums[end]
                 while(start < end) {
                     int sum = nums[start] + nums[end];
                     if(sum > target) {
@@ -20,12 +21,12 @@ public:
                     else {
                         vector<int> v = {nums[i], nums[start], nums[end]};
                         res.push_back(v);
-
+                        
                         while(start < end && nums[start] == nums[start+1]) start++;
                         while(start < end && nums[end] == nums[end-1]) end--;
                         start++;
                         end--;
-                    }   
+                    }
                 }
             }
         }
