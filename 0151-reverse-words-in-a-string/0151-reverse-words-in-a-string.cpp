839ca29753e7bed0f0459f0d5b04cc75aed89e1b
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2022-12-20 21:32:00 +0530

    Time: 11 ms (57.01%), Space: 7.2 MB (80.49%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream SS(s);
-        string token = "";
-        string res = "";
-        while (SS >> token) {
-            res = token + " " + res;
+        //reverse the whole string
+        reverse(s.begin(), s.end());
+        
+        int i=0, l=0, r=0;
+        int n = s.size();
+        while(i < n) {
+            while(s[i] != ' ' && i < n) { // i ko agar char dikha to vo r ko dega and i++ and r++
+                s[r] = s[i];
+                r++;
+                i++;
+            }
+            
+            if(l < r) {
+                reverse(s.begin()+l, s.begin()+r);
+                s[r] = ' ';
+                r++;
+                l = r;
+            }
+            i++;
         }
-        return res.substr(0, res.size()-1);
+        return s.substr(0, r-1);
     }
 };
+
+// https://www.youtube.com/watch?v=mdej8UiRCkI
