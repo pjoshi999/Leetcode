ab3315884a921702dbba6afca79604dbb9699e36
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2022-12-21 01:30:28 +0530

    Time: 47 ms (61.99%), Space: 14.8 MB (55.98%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        return num.substr(0, num.find_last_of("13579")+1);
+        for(int i=num.size()-1; i>=0; i--) {
+            if(int(num[i]) % 2 == 1) {
+                return num.substr(0, i+1);
+            }
+        }
+        return "";
     }
 };
