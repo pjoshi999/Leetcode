3e9c55ea0646877a4370138775e16f10c198793a
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2022-12-21 17:07:29 +0530

    Time: 26 ms (55.34%), Space: 8.5 MB (67.20%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -6,21 +6,17 @@ public:
         for(int i=0; i<s.size(); i++) {
             mp[s[i]]++;
         }
-        
         vector<pair<int, char>> v;
-        for(auto it=mp.begin(); it!= mp.end(); it++) {
-            v.push_back({it->second, it->first});
+        for(auto it: mp) {
+            v.push_back({it.second, it.first});
         }
-        sort(v.begin(), v.end());
-        
+        sort(v.begin(), v.end(), greater<pair<int, char>>());
         for(auto it: v) {
             while(it.first > 0) {
                 res += it.second;
                 it.first -= 1;
             }
         }
-        cout<<res<<endl;
-        reverse(res.begin(), res.end());
         
         return res;
     }
