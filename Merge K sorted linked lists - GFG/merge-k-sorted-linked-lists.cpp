2f4ffd5d06a468aff54084dd496bfe5c3f16cb91
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-02-01 15:30:43 +0530

    Added solution - LeetHub

--- Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.cpp
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.cpp
@@ -46,26 +46,24 @@ struct Node
 class Solution{
   public:
     //Function to merge K sorted linked list.
+    Node* merge(Node* l1, Node* l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        if(l1->data < l2->data) {
+            l1->next =  merge(l1->next, l2);
+        }
+        else {
+            l2->next = merge(l1, l2->next);
+        }
+    }
     Node * mergeKLists(Node *arr[], int K)
     {
         // Your code here
-        vector<pair<int, Node*>> v;
-        for(int i=0; i<K; i++) {
-            Node* curr = arr[i];
-            while(curr != NULL) {
-                v.push_back({curr->data, curr});
-                curr = curr->next;
-            }
+        Node* res = arr[0];
+        for(int i=1; i<K; i++) {
+            res = merge(res, arr[i]);
         }
-        if(v.size() == 0) {
-            return NULL;
-        }
-        sort(v.begin(), v.end());
-        for(int i=0; i<v.size()-1; i++) {
-            v[i].second->next = v[i+1].second;
-        }
-        v[v.size()-1].second->next = NULL;
-        return v[0].second;
+        return res;
     }
 };
 
