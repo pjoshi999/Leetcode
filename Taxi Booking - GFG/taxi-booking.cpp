c6b471cb9719b2d85a5006f1f84ccdebc4d370ce
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-03-21 23:52:15 +0530

    Added solution - LeetHub

---
+++ Taxi Booking - GFG/taxi-booking.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int minimumTime(int N,int cur,vector<int> &pos,vector<int> &time){
+        int res = INT_MAX;
+        vector<int> v;
+        for(int i=0; i<pos.size(); i++) {
+            res = min(res, abs(cur-pos[i]) * time[i]);
+            // cout<<res<<endl;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N,cur;
+        cin>>N>>cur;
+        vector<int> pos(N),time(N);
+        for(int i=0;i<N;i++){
+            cin>>pos[i];
+        }
+        for(int i=0;i<N;i++){
+            cin>>time[i];
+        }
+        Solution ob;
+        cout<<ob.minimumTime(N,cur,pos,time)<<endl;
+    }
+}
+// } Driver Code Ends
