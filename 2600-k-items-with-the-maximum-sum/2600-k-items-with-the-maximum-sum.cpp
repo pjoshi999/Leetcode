009592f905c8a949edbcdeca7fc1b1694b6bce8a
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-03-26 18:46:20 +0530

    Time: 11 ms (44.44%), Space: 6.7 MB (5.56%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        vector<int> v;
+        for(int i=0; i<numOnes; i++) v.push_back(1);
+        for(int i=0; i<numZeros; i++) v.push_back(0);
+        for(int i=0; i<numNegOnes; i++) v.push_back(-1);
+        int res = 0;
+        for(int i=0; i<k; i++) {
+            res += v[i];
+        }
+        return res;
+    }
+};
