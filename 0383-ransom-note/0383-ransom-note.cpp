1d13c35c795cda11a1f5e7479cd6f00cbcf850c1
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-03-27 11:42:31 +0530

    Time: 200 ms (5.09%), Space: 44 MB (7.50%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,24 +1,8 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        if(magazine.size() < ransomNote.size()) {
-            return false;
-        }
-        unordered_map<char, int> mp;
-        for(int i=0; i<magazine.size(); i++) {
-            mp[magazine[i]]++;
-        }
-        for(int i=0; i<ransomNote.size(); i++) {
-            if(mp.find(ransomNote[i]) != mp.end()) {
-                mp[ransomNote[i]]--;
-                if(mp[ransomNote[i]] == 0) {
-                    mp.erase(ransomNote[i]);
-                }
-            }
-            else {
-                return false;
-            }
-        }
-        return true;
+        multiset<char> mag(magazine.begin(), magazine.end());
+        multiset<char> ransom(ransomNote.begin(), ransomNote.end());        
+        return includes(mag.begin(), mag.end(), ransom.begin(), ransom.end());
     }
 };
