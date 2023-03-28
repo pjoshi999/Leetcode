2764232aadf7c19a898055abdb9ce28b2d6defd4
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-03-28 20:20:42 +0530

    Added solution - LeetHub

---
+++ Shy Geek - GFG/shy-geek.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Shop
+{
+    vector<int> chocolates;
+    int countOfCalls;
+    public:
+    Shop()
+    {
+        countOfCalls = 0;
+    }
+    void addChocolate(int x)
+    {
+        chocolates.push_back(x);
+    }
+    long long get(int i)
+    {
+        countOfCalls++;
+        if(i<0 || i>=(int)chocolates.size() || countOfCalls>50)return -1;
+        return chocolates[i];
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+/*
+Instructions - 
+
+    1. 'shop' is object of class Shop.
+    2. User 'shop.get(int i)' to enquire about the price
+            of the i^th chocolate.
+    3. Every chocolate in shop is arranged in increasing order
+            i.e. shop.get(i) <= shop.get(i + 1) for all 0 <= i < n - 1
+*/
+class Solution{
+    public:
+    
+    Shop shop;
+    Solution(Shop s)
+    {
+        this->shop = s;
+    }
+    long long find(int n, long k){
+        // Return the number of chocolates Geek had
+        // enjoyed out of 'n' chocolates availabe in the
+        // 'shop'.
+        long long int res = 0;
+        long long int start = 0;
+        long long int end = n-1;
+        while(k > 0 && end >= 0) {
+            start = 0;
+            long long int index = -1;
+            long long int index_value;
+            while(start <= end) {
+                long long int mid = start + (end - start)/2;
+                long long int mid_price = shop.get(mid);
+                if(mid_price <= k) {
+                    index = mid;
+                    index_value = mid_price;
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+            res += k/index_value;
+            k = k % index_value;
+            end = index - 1;
+        }
+        return res;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        long long k;
+        cin >> k;
+        
+        Shop shop;
+        for(int i = 0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            shop.addChocolate(x);
+        }
+        
+        Solution obj(shop);
+        long long res = obj.find(n, k);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
