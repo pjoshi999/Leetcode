35ffa4e0a17334307f9c115e2ec17f081d0580ac
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-04-11 03:25:46 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.10%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        if(s.size() == 0) {
+            return true;
+        }
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
+            }
+            else if( (s[i] == ')' && !st.empty() && st.top() == '(') || 
+                (s[i] == ']' && !st.empty() && st.top() == '[') || 
+                (s[i] == '}' && !st.empty() && st.top() == '{') ) {
+
+                st.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return st.empty(); 
+    }
+};
