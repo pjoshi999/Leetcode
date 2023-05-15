432b0d166e5c9d17112cd89342bac19559e3aa66
Priyanshu Joshi <89994650+pjoshi999@users.noreply.github.com>
2023-05-15 23:39:35 +0530

    Added solution - LeetHub

---
+++ Count Total Setbits - GFG/count-total-setbits.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long countBits(long long N) {
+        // code here
+        long long int res = 0;
+        for(long long int i=1; i<=N; i++) {
+            long long int count1s = __builtin_popcountll(i);
+            res += count1s;
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        long long N;
+        scanf("%lld",&N);
+        
+        Solution obj;
+        long long res = obj.countBits(N);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
