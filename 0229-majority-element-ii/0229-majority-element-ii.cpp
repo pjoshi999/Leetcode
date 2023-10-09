bc0cff162792af402605b6f0f92a31ad72d678b8
Priyanshu Joshi <joshi.priyanshu999@gmail.com>
2023-10-09 06:49:15 +0530

    Time: 10 ms (56.82%), Space: 16.2 MB (78.17%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,52 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int ,int> mp;
-        for(int i=0; i<nums.size(); i++) {
-            mp[nums[i]]++;
-        }
+        // Boyer-Moore Algorithm
+        // As we have to find elements greater than n/3, so only n-1 i.e., 2 elements can be in majority.
         vector<int> res;
-        int temp = 0;
-        for(auto it: mp) {
-            if(it.second > nums.size()/3) {
-                res.push_back(it.first);
+
+        int count1 = 0;
+        int majority_ele1 = 0;
+        int count2 = 0;
+        int majority_ele2 = 0;
+
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] == majority_ele1) {
+                count1++;
+            }
+            else if(nums[i] == majority_ele2) {
+                count2++;
+            }
+            else if(count1 == 0) {
+                majority_ele1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0) {
+                majority_ele2 = nums[i];
+                count2 = 1;
+            }
+            else {
+                count1--;
+                count2--;
             }
         }
-        return res;
+
+        // verification
+        int freq1 = 0;
+        int freq2 = 0;
+        for(int &num: nums) {
+            if(num == majority_ele1) freq1++;
+            else if(num == majority_ele2) freq2++;
+        }
+
+        if(freq1 > floor(nums.size()/3)) {
+            res.push_back(majority_ele1);
+        }        
+        if(freq2 > floor(nums.size()/3)) {
+            res.push_back(majority_ele2);
+        }
+        return  res;
     }
 };
+
+// https://youtu.be/Q6L5SoS-fTo?si=CwO7JtvoETjYPapj
